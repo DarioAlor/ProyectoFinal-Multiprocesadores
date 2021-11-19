88a5286dadb9670f6987027a119e8d5c6d0c5d3e
DarioAlor <A01731643@tec.mx>
2021-11-19 19:42:40 +0000

    gris 1

--- grisParalelo.c
+++ grisParalelo.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 
-#define NUM_THREADS 100
+#define NUM_THREADS 1
 
 int main()
 {
@@ -12,7 +12,7 @@ int main()
 
     FILE *image, *outputImage, *lecturas;
     image = fopen("f9.bmp","rb");          //Imagen original a transformar
-    outputImage = fopen("f9_google.bmp","wb");    //Imagen transformada
+    outputImage = fopen("f9_google1.bmp","wb");    //Imagen transformada
     long ancho;
     long alto;
     unsigned char r, g, b;               //Pixel
